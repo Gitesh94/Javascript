70d5f8ff512154849fb80186d1e0a0fa8ff4e9cc
Gitesh94 <giteshraval047@gmail.com>
2025-07-15 12:01:34 +0530

    Math and random number usage

---
+++ 06_math_and_random.js
@@ -0,0 +1,15 @@
+const num = 100000
+
+// console.log(num.toLocaleString());
+
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10) +1);
+
+const decimalNumber = 101.40
+
+console.log(decimalNumber.toFixed());
+console.log(Math.floor(decimalNumber));
+console.log(Math.ceil(decimalNumber));
+console.log(Math.round(decimalNumber));
+console.log(decimalNumber.toPrecision(4));
+
